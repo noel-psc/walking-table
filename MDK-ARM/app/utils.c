4264c160dea53ae1fbac261e13ab21ab50a7b812
chenhlSH <jxxx16chl@163.com>
2025-11-26 20:47:01 +0800

    显然是字符串处理失败，重写逻辑

--- MDK-ARM/app/utils.c
+++ MDK-ARM/app/utils.c
@@ -1,8 +1,10 @@
 #include "utils.h"
 const float k_=0.1;
 float fof_update(float input) {
-
+	if (input != 0 ){
   input_ = input;
-  output_ = output_ * (1 - k_) + input_ * k_;
+  output_ = output_ * (1 - k_) + input_ * k_;}
   return output_;
+	
 }
+
